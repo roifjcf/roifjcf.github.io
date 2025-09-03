aff4c13c0d8ded7dfeea63130759b71664b70f7d
roifjcf <roifjcf@gmail.com>
2025-09-04 09:13:58 +1000

    feat: SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './src/util/remark-modified-time.mjs';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/
 export default defineConfig({
-  
+  site: 'https://roifjcf.github.io/', // required
+  integrations: [sitemap({
+    
+  })],
   markdown: {
     shikiConfig: {
       theme: 'material-theme-darker',
